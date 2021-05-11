becf4d60853b3cbfb82ba37333273312d35ebcb9
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-11 17:06:24 +0000

    Moved response headers to separate file

---
+++ ResponseHeaders.php
@@ -0,0 +1,7 @@
+<?php
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE,HEAD");
+        header("Access-Control-Max-Age: 3600");
+        header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization,X-Requested-With");
+?>
--- index.php
+++ index.php
@@ -3,18 +3,14 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);  
 
-    header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE,HEAD");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization,X-Requested-With");
+    require_once __DIR__."/ResponseHeaders.php";
+
+    define("BASE_URL", "http://localhost:8000/api/v1/");
     
-    // $routesMapper = new \stdClass();
-    // $routesMapper->transactions = "http://localhost/transactions/";
-    // $routesMapper->name = "https://github.com/Nyamador/payment-gateway-coursework-php";
     $routesMapper = array(
-        'transactions' => "http://localhost/transactions/",
-        "name" => "https://github.com/Nyamador/payment-gateway-coursework-php"
+        "transactions" => BASE_URL."transactions/",
+        "otps" => BASE_URL."otps/",
+        "links" => BASE_URL.""
     );
     echo json_encode($routesMapper, JSON_PRETTY_PRINT);
 ?>
