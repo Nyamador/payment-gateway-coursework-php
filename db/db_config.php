0bc42b829aec93740671e01765660c6bc4d01016
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-11 14:02:02 +0000

    First commit

---
+++ db/db_config.php
@@ -0,0 +1,30 @@
+<?php
+    class PDOConnection{
+        private static $pdo;
+        private static $host = 'localhost';
+        private static $user = 'root';
+        private static $dbname = 'gateway';
+        private static $password = '';
+
+        public static function instance()
+        {
+            if (PDOConnection::$pdo === null) {
+                self::init();
+            }
+    
+            return self::$pdo;
+        }    
+        
+
+        private static function init()
+        {
+            $opt = array(
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                PDO::ATTR_EMULATE_PREPARES   => FALSE,
+            );
+            $dsn = 'mysql:host='.self::$host.';dbname='.self::$dbname.";charset=utf8";
+            self::$pdo = new PDO($dsn, self::$user, self::$password, $opt);
+        }        
+    }
+?>
