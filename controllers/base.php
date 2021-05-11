892fc98678b20b35cffe20f1052ba25ac829c68b
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-11 17:05:46 +0000

    Added custom controller

---
+++ controllers/base.php
@@ -0,0 +1,15 @@
+<?php
+    abstract class BaseController{
+        private $model;
+        private $requestMethod;
+        private $userId;
+
+        public function __construct($model, $requestMethod, $userId) {
+                $this->model = $model;
+                $this->requestMethod = $requestMethod;
+                $this->userID = $userId;
+        }
+
+        public function handleRequest(){}
+    }
+?>
