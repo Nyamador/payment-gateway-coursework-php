a613e728b94936410c59ff1adac930399b6a44be
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-14 14:00:36 +0000

    File for mounting react

---
+++ pay.php
@@ -0,0 +1,22 @@
+<?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);  
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./build/main.css">
+  <title>React</title>
+</head>
+<body>
+  <div id="root">
+
+  </div>
+    <script src="./build/main.js"></script>
+</body>
+</html>
