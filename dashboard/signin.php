c0c50c2b036d0b76ea85ff7ccd94c6879086afc6
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-19 01:21:49 +0000

    Added dashboard

---
+++ dashboard/signin.php
@@ -0,0 +1,31 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once __DIR__."/../models/User.php";
+    require_once __DIR__."/../db/db_config.php";
+    
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $userModel = new User(PDOConnection::instance());   
+        if($userModel->exists($_POST["email"])){
+            // start a session and append users details to session
+            $userData = $userModel->find($_POST["email"]);
+            if(password_verify($_POST['password'], $userData->password)){
+                session_start();
+                $_SESSION['email'] = $userData->email;
+                $_SESSION['uid'] = $userData->id;
+                $_SESSION['isAuthenticated'] = true;
+                header("Location: ../dashboard.php");
+            }else{
+                header("Location: ../signin.php?error=true&detail=Incorrect password&email={$_POST['email']}");
+            }
+        }else{
+            // ask user to login again
+            session_destroy();
+            header("Location: ../signin.php?error=true&detail=User Does Not Exist");
+        };
+    }
+
+?>
---
+++ dashboard/signup.php
@@ -0,0 +1,19 @@
+<?php
+    require_once __DIR__."/../models/User.php";
+    require_once __DIR__."/../db/db_config.php";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $email =  $_POST['email'];
+        $password =  $_POST['password'];
+        $userModel = new User(PDOConnection::instance());
+        if(!$userModel->exists($email)){
+            $userModel->insert(array('email' => $email, 'password' => password_hash($password, PASSWORD_DEFAULT)));
+            session_start();
+            $_SESSION['email'] = $email;
+            $_SESSION['isAuthenticated'] = TRUE;
+            header('Location: ../dashboard.php');  
+        }else{
+            header("Location: ../signup.php?error=User Already Exists");
+        }
+    }
+?>
