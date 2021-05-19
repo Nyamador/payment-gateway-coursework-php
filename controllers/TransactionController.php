a1f6665ffb4309734556c013fa7615a9bc4020ac
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-19 23:45:46 +0000

    Added Transaction Controller

--- controllers/TransactionController.php
+++ controllers/TransactionController.php
@@ -2,27 +2,30 @@
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);  
+    error_reporting(E_ALL); 
 
     require_once __DIR__."/base.php";
     require_once __DIR__."/../models/Link.php";
     require_once __DIR__."/../models/User.php";
+    require_once __DIR__."/../models/Transaction.php";
     require_once __DIR__."/../db/db_config.php";
-
-    class LinkController extends BaseController{
+    
+    
+    class TransactionController extends BaseController{
         public function __construct($requestMethod, $api_key){
             parent::__construct(new LinkModel(PDOConnection::instance()), $requestMethod, $api_key);
         }
 
-    
-
         public function handleRequest(){
+            header('Content-type: application/json');
+            $PDOSocket = PDOConnection::instance();
+            $userModel = new User($PDOSocket);
+            $linkModel = new LinkModel($PDOSocket);
+            $transactionModel = new Transaction($PDOSocket);
             switch($this->requestMethod){
                 case 'GET':
-                    // get user id from the token
-                    header('Content-Type: application/json');
-                    $userModel = new User(PDOConnection::instance());
-                    $userData = $userModel->findByToken($this->user_token);
+                    break;
+                case 'POST':
                     if (isset($userData['api_key'])){
                         if (isset($_GET['id'])){
                             $response = $this->model->find($_GET['id']);
@@ -33,27 +36,27 @@
                             http_response_code(401);
                             $responseObj = array('error' => true, 'message' => "Unauthorized! Authorization Credentials Not found");
                             echo json_encode($responseObj); 
+
                     }
                     break;
                 case 'POST':
-                    header('Content-type: application/json');
-                    $userModel = new User(PDOConnection::instance());
-                    $userData = $userModel->findByToken($this->user_token);
                     $data = json_decode(file_get_contents('php://input'), true);
                     $uniqId = uniqid("");
+                    $userData = $userModel->findByToken($this->user_token);
+                    $linkData = $linkModel->find($_GET['id']);
                     if(isset($userData['api_key'])){
                         $insertedData = $this->model->insert(array(
-                            'uuid' => $uniqId,
-                            'user_id' => $userData['id'],
+                            'user' => $userData['id'],
+                            'link' => $linkData['id'],
+                            'country' => 'Ghana',
                             'customer_name' => $data["customer_name"],
                             'customer_email' => $data['customer_email'],
                             'customer_mobile' => $data['customer_mobile'],
                             'amount' => $data['amount'],
-                            'success_url' => $data['success_url'],
-                            'error_url' => $data['error_url'],
+                            'payment_method' => $data['payment_method']
                         ));
                         if ($insertedData){
-                            $responseObj = array('success' => true, 'link' => 'http://localhost:8000/pay.php?token=?'.$uniqId);
+                            $responseObj = array('success' => true, 'message' => 'Transaction Created successfully');
                             echo json_encode($responseObj);
                         }else{
                             $responseObj = new \stdClass();
@@ -65,7 +68,7 @@
                         http_response_code(401);
                         $responseObj = array('error' => true, 'message' => "Unauthorized. Authorization Credentials Not found");
                         echo json_encode($responseObj);                        
-                    }
+                    }                    
                     break;
                 default:
                     header('Content-type: application.json');
@@ -74,5 +77,7 @@
             }
         }
 
+
     }
+
 ?>
