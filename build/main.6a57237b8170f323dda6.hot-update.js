3cfc5e6e70bc6509e17d398195947fd3188bc360
Desmond Nyamador <dselasi4u@gmail.com>
2021-05-19 01:21:31 +0000

    Frontend build

---
+++ build/main.6a57237b8170f323dda6.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("main",{
+
+/***/ "./frontend/components/CardForm.js":
+/*!*****************************************!*\
+  !*** ./frontend/components/CardForm.js ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/desmond/projects/covuni/webapi-php/frontend/components/CardForm.js: Unexpected token (119:16)\n\n\u001b[0m \u001b[90m 117 |\u001b[39m                 {isLoading \u001b[33m?\u001b[39m \u001b[0m\n\u001b[0m \u001b[90m 118 |\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 119 |\u001b[39m                 \u001b[33m:\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m     |\u001b[39m                 \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 120 |\u001b[39m                 \u001b[33m<\u001b[39m\u001b[33mButton\u001b[39m disabled\u001b[33m=\u001b[39m{isLoading}\u001b[33m>\u001b[39m{isLoading \u001b[33m?\u001b[39m \u001b[33m<\u001b[39m\u001b[33mLoaderIcon\u001b[39m\u001b[33m/\u001b[39m\u001b[33m>\u001b[39m \u001b[33m:\u001b[39m \u001b[32m\"Pay GHC 145.00\"\u001b[39m}\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mButton\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 121 |\u001b[39m                 }\u001b[0m\n\u001b[0m \u001b[90m 122 |\u001b[39m             \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mform\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n    at Object._raise (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:776:17)\n    at Object.raiseWithData (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:769:17)\n    at Object.raise (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:737:17)\n    at Object.unexpected (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:9675:16)\n    at Object.parseExprAtom (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:11069:20)\n    at Object.parseExprAtom (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:5177:20)\n    at Object.parseExprSubscripts (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:10644:23)\n    at Object.parseUpdate (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:10624:21)\n    at Object.parseMaybeUnary (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:10602:23)\n    at Object.parseExprOps (/home/desmond/projects/covuni/webapi-php/node_modules/@babel/parser/lib/index.js:10463:23)");
+
+/***/ }),
+
+/***/ "./frontend/icons/loading.svg":
+false,
+
+/***/ "./node_modules/creditcard.js/dist/creditcard.js":
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWluLjZhNTcyMzdiODE3MGYzMjNkZGE2LmhvdC11cGRhdGUuanMiLCJzb3VyY2VSb290IjoiIn0=
